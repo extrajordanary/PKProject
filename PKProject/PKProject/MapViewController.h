84cfdeaf82fd36068af404579e3bf7b5294dd8ff
Jordan Arnesen <jordan101112@gmail.com>
2014-09-30 14:55:24 -0700

    creating initial storyboard layout for main map view

--- PKProject/PKProject/MapViewController.h
+++ PKProject/PKProject/MapViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  MapViewController.h
 //  PKProject
 //
-//  Created by Jordan on 9/29/14.
+//  Created by Jordan on 9/30/14.
 //  Copyright (c) 2014 Byjor. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MapViewController : UIViewController
 
 @end
-
