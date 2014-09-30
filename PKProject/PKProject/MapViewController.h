39e8d9698a696df834064d8fe7160018b11b7cb8
Jordan Arnesen <jordan101112@gmail.com>
2014-09-30 15:51:26 -0700

    basic map loading and getting user authorization for location tracking; zooms to user location

--- PKProject/PKProject/MapViewController.h
+++ PKProject/PKProject/MapViewController.h
@@ -7,7 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface MapViewController : UIViewController
+@interface MapViewController : UIViewController  <MKMapViewDelegate, CLLocationManagerDelegate>
 
 @end
--- PKProject/PKProject/MapViewController.m
+++ PKProject/PKProject/MapViewController.m
@@ -10,18 +10,79 @@
 
 @interface MapViewController ()
 
+@property (strong, nonatomic) IBOutlet MKMapView *mainMap;
+@property (strong, nonatomic) CLLocationManager *locationManager;
+
 @end
+
+#define METERS_PER_MILE 1609.344
+#define DEFAULT_ZOOM_MILES .5
 
 @implementation MapViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self startStandardUpdates];
+    
+    self.mainMap.showsUserLocation = YES;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Location Manager
+
+- (BOOL)startStandardUpdates
+{
+    // Create the location manager if this object does not already have one.
+    if (nil == self.locationManager) {
+        self.locationManager = [[CLLocationManager alloc] init];
+    }
+    
+    self.locationManager.delegate = self;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+    
+    // Set a movement threshold for new events.
+    self.locationManager.distanceFilter = 500; // meters
+    
+    [self.locationManager requestAlwaysAuthorization];
+    
+    [self.locationManager startUpdatingLocation];
+    
+    return YES;
+}
+
+// delegate method called when user changes authorization to allow location tracking
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    if (status) {
+        MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(self.locationManager.location.coordinate, DEFAULT_ZOOM_MILES*METERS_PER_MILE, DEFAULT_ZOOM_MILES*METERS_PER_MILE);
+        [self.mainMap setRegion:viewRegion animated:YES];
+    }
+}
+
+// Delegate method from the CLLocationManagerDelegate protocol.
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    // If it's a relatively recent event, turn off updates to save power.
+    CLLocation* location = [locations lastObject];
+    
+    //    NSDate* eventDate = location.timestamp;
+    //    NSTimeInterval howRecent = [eventDate timeIntervalSinceNow];
+    
+//        if (abs(howRecent) < 15.0) {
+    // If the event is recent, do something with it.
+    NSLog(@"latitude %+.6f, longitude %+.6f\n",
+          location.coordinate.latitude,
+          location.coordinate.longitude);
+//        }
 }
 
 /*
