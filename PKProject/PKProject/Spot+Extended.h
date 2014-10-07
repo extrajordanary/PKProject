51cb6fb82bdd36d786aaaed3e2b7676734709955
Jordan Arnesen <jordan101112@gmail.com>
2014-10-07 10:25:55 -0700

    adding sample pics from Boston; sample server data

---
+++ PKProject/PKProject/Spot+Extended.h
@@ -0,0 +1,13 @@
+//
+//  Spot+Extended.h
+//  PKProject
+//
+//  Created by Jordan on 10/3/14.
+//  Copyright (c) 2014 Byjor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Spot_Extended : NSObject
+
+@end
---
+++ PKProject/PKProject/Spot+Extended.m
@@ -0,0 +1,13 @@
+//
+//  Spot+Extended.m
+//  PKProject
+//
+//  Created by Jordan on 10/3/14.
+//  Copyright (c) 2014 Byjor. All rights reserved.
+//
+
+#import "Spot+Extended.h"
+
+@implementation Spot_Extended
+
+@end
