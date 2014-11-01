f0903c81dc7b33316ce22d0b9fa3988b521af8e1
Jordan Arnesen <jordan101112@gmail.com>
2014-10-31 17:46:47 -0700

    starting AWSHandler

---
+++ PKProject/PKProject/AWSHandler.h
@@ -0,0 +1,13 @@
+//
+//  AWSHandler.h
+//  PKProject
+//
+//  Created by Jordan on 10/31/14.
+//  Copyright (c) 2014 Byjor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AWSHandler : NSObject
+
+@end
---
+++ PKProject/PKProject/AWSHandler.m
@@ -0,0 +1,13 @@
+//
+//  AWSHandler.m
+//  PKProject
+//
+//  Created by Jordan on 10/31/14.
+//  Copyright (c) 2014 Byjor. All rights reserved.
+//
+
+#import "AWSHandler.h"
+
+@implementation AWSHandler
+
+@end
